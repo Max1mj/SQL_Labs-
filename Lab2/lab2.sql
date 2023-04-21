975a47a88e618363842d8ded5ead982226c63adb
Maksym <jmaximuaj@gmail.com>
2023-04-21 07:56:20 +0100

    Lab2

---
+++ Lab2/lab2.sql
@@ -0,0 +1,33 @@
+
+SELECT id,name,population
+FROM cities
+WHERE population > 1000000;
+
+
+
+SELECT name,region,population
+FROM cities
+WHERE region IN ('W','E')
+ORDER BY population DESC;
+
+
+
+SELECT name,region,population
+FROM cities
+WHERE region IN ('S','C','N') AND population > 500000;
+
+
+SELECT id,name,region,population
+FROM cities
+WHERE region IN ('E','W','N') AND population > 150000 AND population < 350000
+ORDER BY name DESC;
+
+
+
+FROM cities
+WHERE NOT region IN ('E','W')
+ORDER BY population DESC
+LIMIT 10 OFFSET 10;
+
+
+
