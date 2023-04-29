4cb97f5cbf74e3bf1e7a09c7d74f38aa5011cc06
Maksym <jmaximuaj@gmail.com>
2023-04-29 18:09:14 +0100

    changed names to latin

--- Lab3/lab3.sql
+++ Lab3/lab3.sql
@@ -8,20 +8,20 @@ FROM cities
 WHERE name LIKE'%донец%';
 
 
-SELECT id,Concat(name,'(',region,')') AS НАЗВА_МІСТА(РЕГІОН),population
+SELECT id,Concat(name,'(',region,')') AS name_region,population
 FROM cities
 WHERE population > 100000
 ORDER BY name ASC;
 
 
 
-SELECT id,name,population,Concat((population/40000000*100),'%') AS ПРОЦЕНТ_НАСЕЛЕННЯ
+SELECT id,name,population,Concat((population/40000000*100),'%') AS persc_ofpopul
 FROM cities 
 ORDER BY population DESC;
 
 
-SELECT id,name,population,Concat((population/40000000*100),'%') AS ПРОЦЕНТ_НАСЕЛЕННЯ
+SELECT id,name,population,Concat((population/40000000*100),'%') AS persc_popul
 FROM cities 
 WHERE population > 100000
-ORDER BY  ПРОЦЕНТ_НАСЕЛЕННЯ DESC;
+ORDER BY  persc_popul DESC;
 
